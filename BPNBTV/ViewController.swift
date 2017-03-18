7137aa00c9e624ca786ea30950811d04509d25cd
Raditya Maulana <radithmix@gmail.com>
2017-03-18 14:06:32 +0700

    Initial Commit

---
+++ BPNBTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BPNBTV
+//
+//  Created by Raditya Maulana on 3/18/17.
+//  Copyright © 2017 Radith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
